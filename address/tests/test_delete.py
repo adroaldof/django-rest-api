0d949b2a11592a86e45db2743ec194b5d8ede3f7
Adroaldo de Andrade <adroaldof@gmail.com>
2020-11-03 21:13:00 -0300

    Use functions from test setup

--- address/tests/test_delete.py
+++ address/tests/test_delete.py
@@ -2,17 +2,13 @@ from django.urls import reverse
 from rest_framework.views import status
 from rest_framework.test import APIClient, APITestCase
 
-from .setup import TestSetup, create_address
-
-
-def get_url(key):
-    return reverse("address:retrieve_update_destroy", kwargs={"pk": key})
+from .setup import TestSetup, create_address, get_url_by_key
 
 
 class DeleteAddressTestsNotAuthenticated(APITestCase):
     def test_returns_not_authorized(self):
         address = create_address()
-        url = get_url(address.id)
+        url = get_url_by_key(address.id)
 
         client = APIClient()
         response = client.delete(url)
@@ -22,14 +18,14 @@ class DeleteAddressTestsNotAuthenticated(APITestCase):
 
 class DeleteAddressTestsAuthenticated(TestSetup):
     def test_returns_404_not_found(self):
-        url = get_url(999)
+        url = get_url_by_key(999)
         response = self.client.delete(url)
 
         self.assertEqual(response.status_code, status.HTTP_404_NOT_FOUND)
 
     def test_deletes_an_existing_address(self):
         address = create_address()
-        url = get_url(address.id)
+        url = get_url_by_key(address.id)
         response = self.client.delete(url)
 
         self.assertEqual(response.status_code, status.HTTP_204_NO_CONTENT)
--- address/tests/test_update.py
+++ address/tests/test_update.py
@@ -1,21 +1,16 @@
 import json
 
-from django.urls import reverse
 from rest_framework.views import status
 from rest_framework.test import APIClient, APITestCase
 
-from .setup import TestSetup, create_address
-
-
-def get_url(key):
-    return reverse("address:retrieve_update_destroy", kwargs={"pk": key})
+from .setup import TestSetup, create_address, get_url_by_key
 
 
 class UpdateAddressTestsNotAuthenticated(APITestCase):
     def test_returns_401_not_authorized(self):
         client = APIClient()
         data = {"address": "new address"}
-        response = client.patch(get_url(1), data=data)
+        response = client.patch(get_url_by_key(1), data=data)
 
         self.assertEqual(response.status_code, status.HTTP_401_UNAUTHORIZED)
 
@@ -23,7 +18,7 @@ class UpdateAddressTestsNotAuthenticated(APITestCase):
 class UpdateAddressTestsAuthenticated(TestSetup):
     def test_returns_404_not_found_when_address_not_exists(self):
         data = {"address": "new address"}
-        url = get_url(999)
+        url = get_url_by_key(999)
         response = self.client.patch(url, data=data)
         content = json.loads(response.content)
 
@@ -33,7 +28,7 @@ class UpdateAddressTestsAuthenticated(TestSetup):
     def test_partially_updates_an_address(self):
         address = create_address()
 
-        url = get_url(address.id)
+        url = get_url_by_key(address.id)
         data = {"address": "new address"}
         response = self.client.patch(url, data=data)
         content = json.loads(response.content)
